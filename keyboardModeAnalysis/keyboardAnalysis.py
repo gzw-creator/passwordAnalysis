cad3bc7252e4906834f0778106a521b4dd2a44cb
creator_gzw <1335699428@qq.com>
2022-10-17 22:18:32 +0800

    pic generate added

--- keyboardModeAnalysis/keyboardAnalysis.py
+++ keyboardModeAnalysis/keyboardAnalysis.py
@@ -1,19 +1,19 @@
 from tqdm import tqdm
 import collections
+
 KeyboardRow1 = ["`", "1", "2", "3", "4", "5", "6", "7", "8", "9", "0", "-", "=", ""]
-KeyboardRow2 = ["", "q", "w", "e", "r", "t", "y", "u", "i", "o", "p", "[", "]", ""] 
-KeyboardRow3 = ["", "a", "s", "d", "f", "g", "h", "j", "k", "l", ";", "", "", ""] 
+KeyboardRow2 = ["", "q", "w", "e", "r", "t", "y", "u", "i", "o", "p", "[", "]", ""]
+KeyboardRow3 = ["", "a", "s", "d", "f", "g", "h", "j", "k", "l", ";", "", "", ""]
 KeyboardRow4 = ["", "z", "x", "c", "v", "b", "n", "m", ",", ".", "/", "", "", ""]
 KeyboardRow1S = ["~", "!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "_", "+", ""]
 KeyboardRow2S = ["", "Q", "W", "E", "R", "T", "Y", "U", "I", "O", "P", "{", "}", "|"]
-KeyboardRow3S = ["","A", "S", "D", "F", "G", "H", "J", "K", "L", ":", "", "", ""] 
+KeyboardRow3S = ["", "A", "S", "D", "F", "G", "H", "J", "K", "L", ":", "", "", ""]
 KeyboardRow4S = ["", "Z", "X", "C", "V", "B", "N", "M", "<", ">", "?", "", "", ""]
 Rows = [KeyboardRow1, KeyboardRow2, KeyboardRow3, KeyboardRow4, \
         KeyboardRow1S, KeyboardRow2S, KeyboardRow3S, KeyboardRow4S]
 
 
 def ConvertToCoordinates(Password):
-
     Coordinates = []
     for c in Password:
         for i, r in enumerate(Rows):
@@ -22,13 +22,18 @@ def ConvertToCoordinates(Password):
             except:
                 pass
     return Coordinates
+
+
 print(ConvertToCoordinates("qwer"))
 
+
 def isAdjacent(Coord1, Coord2):
-    if (abs(Coord1[0] - Coord2[0]) > 1 or abs(Coord1[1] - Coord2[1]) > 1) or (abs(Coord1[0] - Coord2[0]) == 0 and abs(Coord1[1] - Coord2[1]) == 0):
+    if (abs(Coord1[0] - Coord2[0]) > 1 or abs(Coord1[1] - Coord2[1]) > 1) or (
+            abs(Coord1[0] - Coord2[0]) == 0 and abs(Coord1[1] - Coord2[1]) == 0):
         return False
     else:
         return True
+
 
 def CheckForAdjacency(Coordinates):
     Adjacent = 0
@@ -39,8 +44,8 @@ def CheckForAdjacency(Coordinates):
         if isAdjacent(Coordinates[i], Coordinates[i + 1]):
             Adjacent += 1
             strTmp = strTmp + Rows[Coordinates[i][0]][Coordinates[i][1]]
-            if i+1 == len(Coordinates)-1:
-                strTmp = strTmp + Rows[Coordinates[i+1][0]][Coordinates[i+1][1]]
+            if i + 1 == len(Coordinates) - 1:
+                strTmp = strTmp + Rows[Coordinates[i + 1][0]][Coordinates[i + 1][1]]
                 Adjacent += 1
             strTmpDict[strTmp] = Adjacent
         else:
@@ -51,7 +56,7 @@ def CheckForAdjacency(Coordinates):
             Adjacent = 0
             strTmp = ''
     # print(strTmpDict)
-    if len(strTmpDict)>0:
+    if len(strTmpDict) > 0:
         maxLength = max(strTmpDict.values())
         for key in strTmpDict:
             if strTmpDict[key] == maxLength:
@@ -59,39 +64,62 @@ def CheckForAdjacency(Coordinates):
     else:
         return "null"
 
+
 print(CheckForAdjacency(ConvertToCoordinates("1qwertyuio")))
 
 print("ok")
 
 save_path = "./key_result.csv"
-object_list=[]
-with open("D:\mine\passwordAnalysis\passwordAnalysis\keyboardModeAnalysis\data\plain_yahoo.csv") as file:   #读入文件
+save_path_2 = "./key_result2.csv"
+object_list = []
+with open("D:\mine\passwordAnalysis\passwordAnalysis\keyboardModeAnalysis\data\plain_yahoo.csv") as file:  # 读入文件
 
-    for line in file:   #逐行读入
-        #print(line)
+    for line in file:  # 逐行读入
+        # print(line)
 
-        for i in range(1,100):#tqdm(range(1,100)) :
-            if line[i:i+1]=='"':
+        for i in range(1, 100):  # tqdm(range(1,100)) :
+            if line[i:i + 1] == '"':
                 break
-        for j in tqdm(range(i+1,100)):    #根据第二个引号找到passwd
-            if line[j:j+1]=='"':
-                passwd=line[j+1:]
-                #print(passwd)
-                #fen=wordninja.split(passwd)     #对passwd分词
+        for j in tqdm(range(i + 1, 100)):  # 根据第二个引号找到passwd
+            if line[j:j + 1] == '"':
+                passwd = line[j + 1:]
+                # print(passwd)
+                # fen=wordninja.split(passwd)     #对passwd分词
                 fen = CheckForAdjacency(ConvertToCoordinates(passwd))
-                if fen!="null":
+                if fen != "null":
                     object_list.append(fen)
                 break
     print(object_list)
+    object_list_2 = []
+    for ob in object_list:
+        if len(ob) > 4:
+            if ob.isdigit() is not True:
+                object_list_2.append(ob)
+
+    # print(object_list_2)
+    word_counts_2 = collections.Counter(object_list_2)  # 对分词做词频统计
+    word_counts_2 = word_counts_2.most_common(8)  # 获取前10最高频的词
+    print(word_counts_2)  # 输出检查
+    with open(save_path_2, 'w') as sfile:
+        # sfile.writelines("key,frequency"+'\n')
+        pabar = tqdm(word_counts_2)
+        for i in pabar:
+            in_str = str(i).strip('(').strip(')').strip() + '\n'
+            sfile.writelines(in_str)
+            pabar.set_description("2 Saving!")
+        sfile.close()
+    file.close()
+
     word_counts = collections.Counter(object_list)  # 对分词做词频统计
-    word_counts = word_counts.most_common(100)  # 获取前10最高频的词
+    word_counts = word_counts.most_common(20)  # 获取前10最高频的词
     print(word_counts)  # 输出检查
-    with open(save_path,'w') as sfile:
+    with open(save_path, 'w') as sfile:
+        # sfile.writelines("key,frequency"+'\n')
         pabar = tqdm(word_counts)
         for i in pabar:
-            in_str = str(i).strip('(').strip(')').strip()+'\n'
+            in_str = str(i).strip('(').strip(')').strip() + '\n'
             sfile.writelines(in_str)
-            pabar.set_description("Saving!")
+            pabar.set_description("1 Saving!")
         sfile.close()
     file.close()
 # print(Rows[1][1])
---
+++ keyboardModeAnalysis/showPic.py
@@ -0,0 +1,30 @@
+import csv
+import matplotlib.pyplot as plt
+import pandas as pd
+
+plt.rcParams['font.sans-serif'] = ['SimHei']
+plt.rcParams['font.family'] = 'sans-serif'
+plt.rcParams['axes.unicode_minus'] = False
+
+data = pd.read_csv('./key_result2.csv', header=None)
+
+#自定义列名，可改
+data.columns=["key","frequency"]
+
+xdata = []
+y1data = []
+y2data = []
+
+#读取列表名
+xdata = data.loc[:, 'key']
+y1data = data.loc[:, 'frequency']
+
+#作图
+plt.plot(xdata, y1data, color='r', marker='o', mec='r', mfc='w', label=u'frequency')
+# plt.plot(xdata, y2data, color='b', marker='o', mec='r', mfc='w', label=u'列名3')  # color可自定义折线颜色，marker可自定义点形状，label为折线标注
+plt.title(u"keyboard", size=10)
+plt.legend()
+plt.xlabel(u'key', size=10)
+plt.ylabel(u'frequency', size=10)
+
+plt.show()
