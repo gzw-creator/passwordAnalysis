50ac63acf56a6fa63c3f3b6724d82e9a0d398b41
wangzhen2022 <1758807295@qq.com>
2022-10-19 20:01:44 +0800

    model输入

--- model输入/structure2.py
+++ model输入/structure2.py
@@ -13,63 +13,63 @@ data2 = pandas.DataFrame(data2)
 
 for i in range(1,len(data0)):
 
-    if len(data0.loc[i][0]) == 1:
+    if len(str(data0.loc[i][0])) == 1:
         data0.loc[[i]].to_csv("../L/L1.txt",mode='a',index=False,header=None,sep=",")
-    elif (len(data0.loc[i][0])==2):
+    elif (len(str(data0.loc[i][0]))==2):
 
         data0.loc[[i]].to_csv("../L/L2.txt",mode='a',index=False,header=None,sep=",")
-    elif (len(data0.loc[i][0])==3):
+    elif (len(str(data0.loc[i][0]))==3):
     
          data0.loc[[i]].to_csv("../L/L3.txt",mode='a',index=False,header=None,sep=",")
-    elif (len(data0.loc[i][0])==4):
+    elif (len(str(data0.loc[i][0]))==4):
   
          data0.loc[[i]].to_csv("../L/L4.txt",mode='a',index=False,header=None,sep=",")
-    elif (len(data0.loc[i][0])==5):
+    elif (len(str(data0.loc[i][0]))==5):
        
          data0.loc[[i]].to_csv("../L/L5.txt",mode='a',index=False,header=None,sep=",")
-    elif (len(data0.loc[i][0])==6):
+    elif (len(str(data0.loc[i][0]))==6):
   
          data0.loc[[i]].to_csv("../L/L6.txt",mode='a',index=False,header=None,sep=",")
-    elif (len(data0.loc[i][0])==7):
+    elif (len(str(data0.loc[i][0]))==7):
       
          data0.loc[[i]].to_csv("../L/L7.txt",mode='a',index=False,header=None,sep=",")
-    elif (len(data0.loc[i][0])==8):
+    elif (len(str(data0.loc[i][0]))==8):
     
          data0.loc[[i]].to_csv("../L/L8.txt",mode='a',index=False,header=None,sep=",")
-    elif (len(data0.loc[i][0])==9):
+    elif (len(str(data0.loc[i][0]))==9):
      
         data0.loc[[i]].to_csv("../L/L9.txt",mode='a',index=False,header=None,sep=",")
-    elif (len(data0.loc[i][0])==10):
+    elif (len(str(data0.loc[i][0]))==10):
     
         data0.loc[[i]].to_csv("../L/L10.txt",mode='a',index=False,header=None,sep=",")
-    elif (len(data0.loc[i][0])==11):
+    elif (len(str(data0.loc[i][0]))==11):
      
         data0.loc[[i]].to_csv("../L/L11.txt",mode='a',index=False,header=None,sep=",")
-    elif (len(data0.loc[i][0])==12):
+    elif (len(str(data0.loc[i][0]))==12):
 
         data0.loc[[i]].to_csv("../L/L12.txt",mode='a',index=False,header=None,sep=",")
-    elif (len(data0.loc[i][0])==13):
+    elif (len(str(data0.loc[i][0]))==13):
        
         data0.loc[[i]].to_csv("../L/L13.txt",mode='a',index=False,header=None,sep=",")
-    elif (len(data0.loc[i][0])==14):
+    elif (len(str(data0.loc[i][0]))==14):
        
         data0.loc[[i]].to_csv("../L/L14.txt",mode='a',index=False,header=None,sep=",")
-    elif (len(data0.loc[i][0])==15):
+    elif (len(str(data0.loc[i][0]))==15):
       
         data0.loc[[i]].to_csv("../L/L15.txt",mode='a',index=False,header=None,sep=",")
-    elif (len(data0.loc[i][0])==16):
+    elif (len(str(data0.loc[i][0]))==16):
         
         data0.loc[[i]].to_csv("../L/L16.txt",mode='a',index=False,header=None,sep=",")
-    elif (len(data0.loc[i][0])==17):
+    elif (len(str(data0.loc[i][0]))==17):
    
         data0.loc[[i]].to_csv("../L/L17.txt",mode='a',index=False,header=None,sep=",")
-    elif (len(data0.loc[i][0])==18):
+    elif (len(str(data0.loc[i][0]))==18):
         
         data0.loc[[i]].to_csv("../L/L18.txt",mode='a',index=False,header=None,sep=",")
-    elif (len(data0.loc[i][0])==19):
+    elif (len(str(data0.loc[i][0]))==19):
 
         data0.loc[[i]].to_csv("../L/L19.txt",mode='a',index=False,header=None,sep=",")
-    elif (len(data0.loc[i][0])==20):
+    elif (len(str(data0.loc[i][0]))==20):
  
         data0.loc[[i]].to_csv("../L/L20.txt",mode='a',index=False,header=None,sep=",")
 
